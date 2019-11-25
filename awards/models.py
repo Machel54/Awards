d6b6e315a78d27e396ca874ce87e5d4233692e87
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-11-25 08:37:08 +0300

    create search view and html where user can search

--- awards/models.py
+++ awards/models.py
@@ -4,14 +4,15 @@ from url_or_relative_url_field.fields import URLOrRelativeURLField
 
 # Create your models here.
 class User(models.Model):
-    user_name = models.CharField(max_length=30)
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
+    user_name = models.CharField(max_length=10)
+    first_name = models.CharField(max_length =10)
+    last_name = models.CharField(max_length =10)
     email = models.EmailField()
    
     def __str__(self):
         return self.user_name 
-
+    class Meta:
+        ordering = ['user_name']
     def save_user(self):
         self.save()
      
@@ -21,7 +22,7 @@ class UserProfile(models.Model):
     bio = HTMLField()
     contact=models.CharField(max_length=12)
     linkedIn =  URLOrRelativeURLField()
-    # projects = models.ForeignKey('Project',on_delete=models.CASCADE,null=True)
+    projects = models.ForeignKey('Project',on_delete=models.CASCADE,null=True)
 
     def save_profile(self):
         self.save()
@@ -35,3 +36,23 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username   
+
+class Project(models.Model):
+    project_title = models.CharField(max_length=10)
+    project_description = HTMLField()
+    landing_page = models.ImageField(upload_to='landing_pages')
+    live_site = URLOrRelativeURLField()
+    user = models.ForeignKey(User,null=True,blank=True,on_delete=models.CASCADE)
+    design = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    usability = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    content = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    vote_submissions = models.IntegerField(default=0)
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    def __str__(self):
+        return self.project_title
--- awards/views.py
+++ awards/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def search_project(request):
+    try:
+        if 'project' in request.GET and request.GET['project']:
+            searched_term = (request.GET.get('project')).title()
+            searched_project = Project.objects.get(project_title__icontains = searched_term.title())
+            return render(request,'search.html',{'project':searched_project})
+    except (ValueError,Project.DoesNotExist):
+        raise Http404()
+
+    return render(request,'search.html')
